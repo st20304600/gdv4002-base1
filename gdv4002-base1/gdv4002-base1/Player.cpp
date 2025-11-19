3d0689b38329a1bf3ca62b571b9fd00d9588b1a8
Darkenedknight34 <mh771148@gmail.com>
2025-11-19 12:16:01 +0000

    Added screen bounce + screenBouce variable in Player.cpp

--- gdv4002-base1/gdv4002-base1/Player.cpp
+++ gdv4002-base1/gdv4002-base1/Player.cpp
@@ -1,11 +1,14 @@
 #include "Player.h"
 #include "Keys.h"
+#include "Engine.h"
 #include <bitset>
 
 using namespace std;
 using namespace glm;
 
 extern bitset<5> keys; //Using extern to access the global keys variable from main.cpp
+
+float screenBounce = 15.0f;
 
 Player::Player(glm::vec2 initPosition, float initOrientation, glm::vec2 initSize, GLuint initTextureID, float mass) : GameObject2D(initPosition, initOrientation, initSize, initTextureID) {
 
@@ -18,22 +21,39 @@ void Player::update(double tDelta) {
 
 	vec2 F = vec2(0.0f, 0.0f); //Initialize force vector
 
+	// Corrected screen boundary checks
+	if (position.y < -getViewplaneHeight() / 2.0f) {
+		F += glm::vec2(0.0f, screenBounce);
+		//position.y = -getViewplaneHeight() / 2.0f; // Prevent going out of bounds
+
+	}
+	if (position.y > getViewplaneHeight() / 2.0f) {
+		F += glm::vec2(0.0f, -screenBounce);
+		//position.y = getViewplaneHeight() / 2.0f; // Prevent going out of bounds
+
+	}
+	if (position.x < -getViewplaneWidth() / 2.0f) {
+		F += glm::vec2(screenBounce, 0.0f);
+		//position.x = -getViewplaneWidth() / 2.0f; // Prevent going out of bounds
+
+	}
+	if (position.x > getViewplaneWidth() / 2.0f) {
+		F += glm::vec2(-screenBounce, 0.0f);
+		//position.x = getViewplaneWidth() / 2.0f; // Prevent going out of bounds
+
+	}
 
 	// 1. accumulate forces
-	if (keys.test(Key::W) == true) {
-
+	if (keys.test(Key::W)) {
 		F += glm::vec2(0.0f, thrust);
 	}
-	if (keys.test(Key::S) == true) {
-
+	if (keys.test(Key::S)) {
 		F += glm::vec2(0.0f, -thrust);
 	}
-	if (keys.test(Key::A) == true) {
-
+	if (keys.test(Key::A)) {
 		F += glm::vec2(-thrust, 0.0f);
 	}
-	if (keys.test(Key::D) == true) {
-
+	if (keys.test(Key::D)) {
 		F += glm::vec2(thrust, 0.0f);
 	}
 
