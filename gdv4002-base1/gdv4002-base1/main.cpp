6d91ccd347606b642f758e138888763e9e80782a
Darkenedknight34 <mh771148@gmail.com>
2025-11-20 04:03:28 +0000

    New branch for assessment. Added Asteroid .cpp and .h

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -2,7 +2,6 @@
 #include "stdlib.h"
 #include "Keys.h"
 #include "Player.h"
-#include "Enemy.h"
 
 #include <bitset>
 
@@ -33,23 +32,6 @@ int main(void) {
 	Player* mainPlayer = new Player(glm::vec2(-1.5f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), playerTexture, 1.0f);
 
 	addObject("player", mainPlayer);
-
-	//Enemy
-	// 1. Load enemy texture 
-	GLuint enemyTexture = loadTexture("Resources\\Textures\\alien01.png");
-
-	// 2. Create enemy objects
-	Enemy* enemy1 = new Enemy(glm::vec2(0.0f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), enemyTexture, 0.0f, glm::radians(45.0f));
-
-	Enemy* enemy2 = new Enemy(glm::vec2(1.0f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), enemyTexture, 0.0f, glm::radians(90.0f));
-
-	Enemy* enemy3 = new Enemy(glm::vec2(2.0f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), enemyTexture, 0.0f, glm::radians(60.0f));
-
-	// Add enemy objects to the engine
-	addObject("enemy1", enemy1);
-	addObject("enemy2", enemy2);
-	addObject("enemy3", enemy3);
-
 
 
 	setKeyboardHandler(myKeyboardHandler);
